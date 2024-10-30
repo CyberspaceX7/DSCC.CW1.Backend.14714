1c7382ea19310c6c6af79e8ea5f9a51b51d877c0
DELL-PC <DELL-PC@DESKTOP-UR1D7RE>
2024-10-30 23:33:29 +0500

    Add project files.

---
+++ DSCC.CW1.Backend.14714/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace DSCC.CW1.Backend._14714
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
